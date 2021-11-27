2b46209e94dd8991c0e2fb4cb4d42afd9f12334d
thallard <thallard@student.42lyon.fr>
2021-11-28 00:15:03 +0100

    First update

---
+++ ex00/Matrix.py
@@ -0,0 +1,18 @@
+
+
+class Matrix(object):
+    def __init__(self, array):
+        self.matrix = []
+
+        print(len(array))
+
+
+    def size(self):
+
+
+        return 1
+
+
+    def is_square(self):
+        length = len(self.matrix)
+
---
+++ ex00/main.py
@@ -0,0 +1,6 @@
+from Matrix import Matrix
+
+
+if __name__ == '__main__':
+    matrix = Matrix([1, 2, 3])
+    print('coucou')
