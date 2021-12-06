05e57806a255cd65b988e28bada9675aa137b373
thallard <thallard@student.42lyon.fr>
2021-12-06 18:30:26 +0100

    Tweaks on dot product

--- ex03/dot.py
+++ ex03/dot.py
@@ -4,10 +4,10 @@ sys.path.append('../')
 
 from Classes.Vector import Vector
 
-
 if __name__ == '__main__':
+    print('\033[35mDot product with vectors\033[0m')
     v1 = Vector([0, 0])
-    v2 = Vector([1, 1])
+    v2 = Vector([1, 0])
     print(v1.dot(v2))
 
     v1 = Vector([1, 1])
