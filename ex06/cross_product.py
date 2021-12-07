391bda026a1be2bc88e8440bb7e753a77aed9f98
thallard <thallard@student.42lyon.fr>
2021-12-07 14:45:54 +0100

    Cross product tests updated

--- ex06/cross_product.py
+++ ex06/cross_product.py
@@ -5,6 +5,7 @@ sys.path.append('../')
 from Classes.Vector import Vector
 
 if __name__ == '__main__':
+    print('\033[35mCross product with vectors (subject)\033[0m')
     v1 = Vector([0, 0, 1])
     v2 = Vector([1, 0, 0])
     print(v1.cross_product(v2.vector))
@@ -15,4 +16,29 @@ if __name__ == '__main__':
 
     v1 = Vector([4, 2, -3])
     v2 = Vector([-2, -5, 16])
+    print(v1.cross_product(v2.vector))
+
+    print('\033[35mCross product with vectors (correction)\033[0m')
+    v1 = Vector([0, 0, 0])
+    v2 = Vector([0, 0, 0])
+    print(v1.cross_product(v2.vector))
+
+    v1 = Vector([1, 0, 0])
+    v2 = Vector([0, 0, 0])
+    print(v1.cross_product(v2.vector))
+
+    v1 = Vector([1, 0, 0])
+    v2 = Vector([0, 1, 0])
+    print(v1.cross_product(v2.vector))
+
+    v1 = Vector([8, 7, -4])
+    v2 = Vector([3, 2, 1])
+    print(v1.cross_product(v2.vector))
+
+    v1 = Vector([1, 1, 1])
+    v2 = Vector([0, 0, 0])
+    print(v1.cross_product(v2.vector))
+
+    v1 = Vector([1, 1, 1])
+    v2 = Vector([1, 1, 1])
     print(v1.cross_product(v2.vector))
