9d11b363cc5d2a338ce9d55c26ab39a49871f8d2
thallard <thallard@student.42lyon.fr>
2022-02-28 15:00:04 +0100

    Tweaks

--- ex12/inverse.py
+++ ex12/inverse.py
@@ -7,12 +7,15 @@ from Classes.Matrix import Matrix
 if __name__ == '__main__':
     print('\033[35mInverse on matrices (subject)\033[0m')
     m1 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    m1.print(False)
     m1.inverse().print()
     
     m1 = Matrix([[2, 0, 0], [0, 2, 0], [0, 0, 2]])
+    m1.print(False)
     m1.inverse().print()
     
     m1 = Matrix([[8, 5, -2], [4, 7, 20], [7, 6, 1]])
+    m1.print(False)
     m1.inverse().print()
 
     # m1 = Matrix([[1, 2], [0, 2]])
